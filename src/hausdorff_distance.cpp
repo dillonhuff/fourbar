f747506bd3f99683c93151ee9bb28119ed30d93a
Dillon Huff <dillonhuff@gmail.com>
2017-02-20 18:05:10 -0800

    Attempt to change hausdorff distance now causing huge values for distance

--- src/hausdorff_distance.cpp
+++ src/hausdorff_distance.cpp
@@ -9,17 +9,23 @@ namespace fourbar {
     assert(curve_1.size() > 0);
     assert(curve_2.size() > 0);
 
-    double max_dist = -1e25;
+    double max_min_dist = -1e25;
     for (auto& pt_1 : curve_1) {
+
+      double min_dist = 1e25;
       for (auto& pt_2 : curve_2) {
 	double d = euclidean_distance(pt_1, pt_2);
-	if (d >= max_dist) {
-	  max_dist = d;
+	if (d <= min_dist) {
+	  min_dist = d;
 	}
+      }
+
+      if (min_dist >= max_min_dist) {
+	max_min_dist = min_dist;
       }
     }
 
-    return max_dist;
+    return max_min_dist;
   }
 
 }
